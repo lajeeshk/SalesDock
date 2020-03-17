8766f9ec818fd429fde7a85060276a4598fd035d
Lajeesh k <lajeeshk@gmail.com>
2020-03-17 17:23:42 +0530

    adding controller and route for product list

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Products;
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $products = Products::all();
+        dd($products);
+        return view('product_list',compact($products));
+    }
+}
