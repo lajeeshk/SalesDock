75f4208c86c361b4ecbb53b2cf68ed48200288d1
Lajeesh k <lajeeshk@gmail.com>
2020-03-17 23:29:32 +0530

    route changed to display product list in homepage

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/products','ProductsController@index');
+Route::get('/','ProductsController@index');
