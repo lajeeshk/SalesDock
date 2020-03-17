6a4090341bdf9b33d096d4ba4ad37b791b507c89
Lajeesh k <lajeeshk@gmail.com>
2020-03-17 21:38:39 +0530

    adding class to get Rules from rule definition files

---
+++ app/Services/RuleSelector.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Services;
+
+class RuleSelector {
+
+    const ruleDefinitionPath = 'Services/RuleDefinitions';
+    const ruleDefinitionNamespace = 'App\Services\RuleDefinitions\\';
+
+    public static function getAllFromFile() :array {
+        
+        $rules = array();
+        $path = app_path().DIRECTORY_SEPARATOR.self::ruleDefinitionPath;
+        $files = scandir($path);
+      
+        foreach($files as $file){
+            if(preg_match("/(.*)\.php$/", $file, $matches)) {
+                $ruleClassName = self::ruleDefinitionNamespace.$matches[1];
+                $ruleObject = new $ruleClassName();
+                $rules[] = $ruleObject->getDefinition();
+            }
+            
+        }
+        return $rules;
+    }
+
+}
